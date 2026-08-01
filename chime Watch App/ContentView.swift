8421ad702d0669930391f1aecab9a76a2a58b8d7
Maximilian Arnold <max@ontora.com>
2026-08-01 16:05:13 +0200

    Initial Commit

---
+++ chime Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  chime Watch App
+//
+//  Created by Maximilian Arnold on 01.08.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ chime Watch App/chimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  chimeApp.swift
+//  chime Watch App
+//
+//  Created by Maximilian Arnold on 01.08.26.
+//
+
+import SwiftUI
+
+@main
+struct chime_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
